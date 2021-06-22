88d5935aedfa654b541828b845ad02245657910c
farzanaa07 <farzanaa98@gmail.com>
2021-06-22 21:47:27 +0100

    view changes

--- FrontEnd/Migrations/20210622201907_migration 22.Designer.cs
+++ FrontEnd/Migrations/20210622201907_migration 22.Designer.cs
@@ -2,14 +2,16 @@
 using FrontEnd;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FrontEnd.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210622201907_migration 22")]
+    partial class migration22
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
